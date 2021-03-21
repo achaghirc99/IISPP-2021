168ffdc46348755333dcb3bbf8d7d1f83bf86aa1
Amine <amichachi@alum.us.es>
2021-03-21 21:33:30 +0100

    Update .travis.yml

--- bbdd_travis.sql
+++ bbdd_travis.sql
@@ -1,6 +1,6 @@
- start transaction;
-  DROP DATABASE IF EXISTS `ebarbbdd`;
-  CREATE DATABASE `ebarbbdd`; 
-  USE `ebarbbdd`;
-  GRANT ALL ON `egc-frontend`.* TO 'user-frontend'@'localhost' IDENTIFIED BY 'us4r-front4nd';
+start transaction;
+  DROP DATABASE IF EXISTS 'ebarbbdd';
+  CREATE DATABASE 'ebarbbdd';
+  USE 'ebarbbdd';
+  GRANT ALL ON 'ebarbbdd'.* TO 'ebar'@'localhost' IDENTIFIED BY 'ebar';
   COMMIT;
